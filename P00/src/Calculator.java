4370143273e36a8a46cdb58ba946df2abf93a547
21027145 <21027145@DESKTOP-CJTV0EF>
2022-06-03 10:20:17 +0800

    First P00 respository

--- P00/src/Calculator.java
+++ P00/src/Calculator.java
@@ -1,7 +1,6 @@
 
 public class Calculator {
 	
-	
 	//add
 	public int add (int a, int b) {
 		 return a + b;
--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class CalculatorTest {
 	int a,b;
 	Calculator cal = new Calculator();
